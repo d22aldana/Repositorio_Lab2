776d0c68ade5f745f2da543669a9906114f1f075
d22al <d22al@LAPTOP-QE2JMOER>
2020-04-04 10:13:09 -0400

    Primer commit and push

---
+++ Solucion_del_lab2/Lab2/Models/Friend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public enum TypeList
+    {
+        Adriana,
+        Adrian,
+        Andrea,
+        Alejandra,
+        Andres
+    }
+    public class Friend
+    {
+        public int FriendId { get; set; }
+        public string Name { get; set; }
+        public TypeList List { get; set; }
+        public string Email { get; set; }
+        public int Birthdate { get; set; }
+
+    }
+}
